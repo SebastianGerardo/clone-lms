3a9270c1182c07fce2bc0d7337e9233f7b41abaa
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-03-31 02:36:42 -0500

    arreglo de ruta de imagen

--- src/pages/Administracion/components/TableCatalogo.jsx
+++ src/pages/Administracion/components/TableCatalogo.jsx
@@ -3,60 +3,60 @@ import React from 'react'
 const TableCatalogo = () => {
   return (
         {/* Section 2 */}
-    <div>
-      <section className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-8">
-          <div>
-            <h1 className="text-2xl font-bold mb-8">Catálogo</h1>
-            <div className="bg-white p-8 rounded-xl shadow-2xl mb-8 flex flex-col gap-8">
+//     <div>
+//       <section className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-8">
+//           <div>
+//             <h1 className="text-2xl font-bold mb-8">Catálogo</h1>
+//             <div className="bg-white p-8 rounded-xl shadow-2xl mb-8 flex flex-col gap-8">
 
-{/* Card 1 */}
-<div className="grid grid-cols-1 xl:grid-cols-4 items-center gap-4 mb-4">
-                <div className="col-span-2 flex items-center gap-4">
-                  <img
-                    src="https://img.freepik.com/foto-gratis/hombre-joven-hermoso-contento-camiseta-azul-que-senala-lado_1262-17845.jpg"
-                    className="w-14 h-14 object-cover rounded-xl"
-                  />
-                  <div>
-                    <h3 className="font-bold">Curso 1</h3>
-                    <p className="text-gray-500">John Díaz</p>
-                  </div>
-                </div>
-                <div>
-                  <span className="bg-green-100 text-green-800 py-1 px-3 rounded-full font-medium">
-                    Nota
-                  </span>
-                </div>
-                <div>
-                  <span className="font-bold"> 20 puntos</span>
-                </div>
-              </div>
-              {/* Card 2 */}
-              <div className="grid grid-cols-1 xl:grid-cols-4 items-center gap-4 mb-4">
-                <div className="col-span-2 flex items-center gap-4">
-                  <img
-                    src="https://img.freepik.com/foto-gratis/alegre-joven-deportista-posando-mostrando-pulgares-arriba-gesto_171337-8194.jpg"
-                    className="w-14 h-14 object-cover rounded-xl"
-                  />
-                  <div>
-                    <h3 className="font-bold">Curso 2</h3>
-                    <p className="text-gray-500">Jhon Philips</p>
-                  </div>
-                </div>
-                <div>
-                  <span className="bg-red-100 text-red-800 py-1 px-3 rounded-full font-medium">
-                    Nota
-                  </span>
-                </div>
-                <div>
-                  <span className="font-bold">16 puntos</span>
-                </div>
-              </div>
-            </div>
+// {/* Card 1 */}
+// <div className="grid grid-cols-1 xl:grid-cols-4 items-center gap-4 mb-4">
+//                 <div className="col-span-2 flex items-center gap-4">
+//                   <img
+//                     src="https://img.freepik.com/foto-gratis/hombre-joven-hermoso-contento-camiseta-azul-que-senala-lado_1262-17845.jpg"
+//                     className="w-14 h-14 object-cover rounded-xl"
+//                   />
+//                   <div>
+//                     <h3 className="font-bold">Curso 1</h3>
+//                     <p className="text-gray-500">John Díaz</p>
+//                   </div>
+//                 </div>
+//                 <div>
+//                   <span className="bg-green-100 text-green-800 py-1 px-3 rounded-full font-medium">
+//                     Nota
+//                   </span>
+//                 </div>
+//                 <div>
+//                   <span className="font-bold"> 20 puntos</span>
+//                 </div>
+//               </div>
+//               {/* Card 2 */}
+//               <div className="grid grid-cols-1 xl:grid-cols-4 items-center gap-4 mb-4">
+//                 <div className="col-span-2 flex items-center gap-4">
+//                   <img
+//                     src="https://img.freepik.com/foto-gratis/alegre-joven-deportista-posando-mostrando-pulgares-arriba-gesto_171337-8194.jpg"
+//                     className="w-14 h-14 object-cover rounded-xl"
+//                   />
+//                   <div>
+//                     <h3 className="font-bold">Curso 2</h3>
+//                     <p className="text-gray-500">Jhon Philips</p>
+//                   </div>
+//                 </div>
+//                 <div>
+//                   <span className="bg-red-100 text-red-800 py-1 px-3 rounded-full font-medium">
+//                     Nota
+//                   </span>
+//                 </div>
+//                 <div>
+//                   <span className="font-bold">16 puntos</span>
+//                 </div>
+//               </div>
+//             </div>
         
          
-          </div>
-        </section>
-    </div>
+//           </div>
+//         </section>
+//     </div>
   )
 }
 
